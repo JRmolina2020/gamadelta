ff6c5d10f1e83c3359e8e7dcb2376af8f9e46f3d
JRmolina2020 <31552404+JRmolina2020@users.noreply.github.com>
2024-03-29 15:00:05 -0500

    cambiosx

--- app/Http/Controllers/API/IncomeController.php
+++ app/Http/Controllers/API/IncomeController.php
@@ -17,38 +17,40 @@ class IncomeController extends Controller
      */
     public function indexData($date)
     {
-        
+
         $incomes = DB::table('incomes as i')
-        ->join('users as u', 'u.id', '=', 'i.user_id')
-        ->join('products as p', 'p.id', '=', 'i.product_id')
-        ->select(
-            'i.id',
-            'i.quantity',
-            'i.date_income',
-            'i.created_at',
-            'p.name as product',
-            'p.id as product_id',
-            'u.name as user'
-        )
-        ->where('i.date_income',$date)
-        ->orderBy('i.id', 'desc')->get();
+            ->join('users as u', 'u.id', '=', 'i.user_id')
+            ->join('products as p', 'p.id', '=', 'i.product_id')
+            ->select(
+                'i.id',
+                'i.quantity',
+                'i.date_income',
+                'i.created_at',
+                'p.name as product',
+                'p.id as product_id',
+                'u.name as user'
+            )
+            ->where('i.date_income', $date)
+            ->orderBy('i.id', 'desc')->get();
         return $incomes;
     }
 
-    public function indexDatatwo ($date,$datetwo){
+    public function indexDatatwo($date, $datetwo)
+    {
         $income = DB::table('incomes as i')
-    ->join('products as p', 'p.id', '=', 'i.product_id')
-    ->select(
-    'p.name as product','p.id',
-    DB::raw('SUM(i.quantity) as quantity'),
-    )
-    ->groupBy('p.name','p.id')
-    ->whereBetween('i.date_income', [$date, $datetwo])
-    ->get();
-    return $income;
+            ->join('products as p', 'p.id', '=', 'i.product_id')
+            ->select(
+                'p.name as product',
+                'p.id',
+                DB::raw('SUM(i.quantity) as quantity'),
+            )
+            ->groupBy('p.name', 'p.id')
+            ->whereBetween('i.date_income', [$date, $datetwo])
+            ->get();
+        return $income;
     }
 
-  
+
     /**
      * Store a newly created resource in storage.
      *
@@ -57,32 +59,30 @@ class IncomeController extends Controller
      */
     public function store(Request $request)
     {
-        $income= new Income;
-        $income->user_id =auth()->id();
+        $income = new Income;
+        $income->user_id = auth()->id();
         $income->product_id = $request['product_id'];
         $income->quantity = $request['quantity'];
         $income->date_income = $request['date_income'];
         $income->save();
-        $id=$income->id;
-        $this->update_stock($id,'add');
+        $id = $income->id;
+        $this->update_stock($id, 'add');
         return response()->json(['message' => 'Entrada registrada'], 200);
-       
     }
 
-    public function update_stock($id,$type)
+    public function update_stock($id, $type)
     {
         $income = Income::find($id);
         $product = Product::find($income->product_id);
-        if($type=='add'){
-        $stock=DB::raw("stock + $income->quantity");
-        $product->stock=$stock;
-        }else{
-        $stock=DB::raw("stock - $income->quantity");
-        $product->stock=$stock;
+        if ($type == 'add') {
+            $stock = DB::raw("stock + $income->quantity");
+            $product->stock = $stock;
+        } else {
+            $stock = DB::raw("stock - $income->quantity");
+            $product->stock = $stock;
         }
-       
+
         $product->save();
-       
     }
 
 
@@ -94,13 +94,11 @@ class IncomeController extends Controller
      */
     public function destroy($id)
     {
-          
+
         $income = Income::find($id);
-        if (!$income) {
-            return response()->json(["message" => "Entrada no encontrado"], 404);
-        }
-        $this->update_stock($id,'delete');
         $income->delete();
+        $this->update_stock($id, 'delete');
+
         return response()->json(["message" => "Entrada eliminada"]);
     }
 }
